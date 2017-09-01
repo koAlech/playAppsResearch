177db0cb628ee61a3196aff30fbb939c82beb706
Alik Hochner <alik.hochner@gmail.com>
2017-09-01 11:23:58 +0300

    Added support to top categories search

--- main.go
+++ main.go
@@ -18,32 +18,34 @@ import (
 	"github.com/gocarina/gocsv"
 )
 
-const SEARCH_URL = "http://%s/api/apps/?fullDetail=true&num=%d&q=%s"
+const MAX_APPS_PER_CALL = 50
 
 type AppSearchResponse struct {
 	Results []AppSearchResult `json:"results"`
 }
 type AppSearchResult struct {
-	Genre          string  `json:"genre" csv:"genre"`
-	GenreID        string  `json:"genreId" csv:"genreId"`
-	AppID          string  `json:"appId" csv:"appId"`
-	Title          string  `json:"title" csv:"title"`
-	Summary        string  `json:"summary" csv:"summary"`
-	DeveloperEmail string  `json:"developerEmail" csv:"developerId"`
-	MinInstalls    int     `json:"minInstalls" csv:"minInstalls"`
-	MaxInstalls    int     `json:"maxInstalls" csv:"maxInstalls"`
-	Score          float64 `json:"score" csv:"score"`
-	Updated        string  `json:"updated" csv:"updated"`
-	AdSupported    bool    `json:"adSupported" csv:"adSupported"`
-	Price          string  `json:"price" csv:"price"`
-	OffersIAP      bool    `json:"offersIAP" csv:"offersIAP"`
+	Genre            string  `json:"genre" csv:"genre"`
+	GenreID          string  `json:"genreId" csv:"genreId"`
+	AppID            string  `json:"appId" csv:"appId"`
+	Title            string  `json:"title" csv:"title"`
+	Summary          string  `json:"summary" csv:"summary"`
+	DeveloperEmail   string  `json:"developerEmail" csv:"developerEmail"`
+	DeveloperAddress string  `json:"developerAddress" csv:"developerAddress"`
+	MinInstalls      int     `json:"minInstalls" csv:"minInstalls"`
+	MaxInstalls      int     `json:"maxInstalls" csv:"maxInstalls"`
+	Score            float64 `json:"score" csv:"score"`
+	Updated          string  `json:"updated" csv:"updated"`
+	AdSupported      bool    `json:"adSupported" csv:"adSupported"`
+	Price            string  `json:"price" csv:"price"`
+	OffersIAP        bool    `json:"offersIAP" csv:"offersIAP"`
 }
 
-var inputFileName, outputFileName, host string
+var inputFileName, outputFileName, host, runType, topCategory string
 var numOfApps int
 
 func init() {
-	flag.StringVar(&inputFileName, "i", "terms.txt", "a file with search terms to process")
+	flag.StringVar(&runType, "t", "", "type of run: [search, top]")
+	flag.StringVar(&inputFileName, "i", "terms.txt", "a file with (search terms or categories [based on https://github.com/facundoolano/google-play-scraper/blob/dev/lib/constants.js#L3]) to process")
 	flag.StringVar(&outputFileName, "o", "results.csv", "a csv that will contain the results")
 	flag.StringVar(&host, "h", "localhost:3000", "the url of the host running google-play-scraper")
 	flag.IntVar(&numOfApps, "n", 5, "the number of apps to return from each search")
@@ -51,42 +53,16 @@ func init() {
 
 func main() {
 	flag.Parse()
-	fmt.Printf("processing file [%s]\n", inputFileName)
 
-	f, err := os.Open(inputFileName)
-	if err != nil {
-		log.Fatal(err)
+	if runType != "top" && runType != "search" {
+		flag.Usage()
+		return
 	}
-	defer f.Close()
 
-	appsHash := make(map[string]bool)
-	apps := []AppSearchResult{}
-
-	r := bufio.NewReader(f)
-	line, err := r.ReadString(10) // line defined once
-	for err != io.EOF {
-
-		url := fmt.Sprintf(SEARCH_URL, host, numOfApps, url.QueryEscape(line))
-
-		resp := &AppSearchResponse{}
-		if err = httpGetJSON(url, resp); err != nil {
-			log.Fatal(err)
-		}
-
-		for _, currApp := range resp.Results {
-			if _, ok := appsHash[currApp.AppID]; !ok {
-				appsHash[currApp.AppID] = true
-				upd, err := convertDate(currApp.Updated)
-				if err != nil {
-					fmt.Printf("convertDate [%s] err: %v", currApp.Updated, err)
-				} else {
-					currApp.Updated = upd
-				}
-				apps = append(apps, currApp)
-			}
-		}
-
-		line, err = r.ReadString(10) //  line was defined before
+	apps, err := getApps()
+	if err != nil {
+		fmt.Printf("Get apps err: %v", err)
+		return
 	}
 
 	if len(apps) > 0 {
@@ -105,19 +81,95 @@ func main() {
 	}
 }
 
-func httpGetJSON(url string, target interface{}) error {
+func getApps() ([]AppSearchResult, error) {
+
+	appsHash := make(map[string]bool)
+	apps := []AppSearchResult{}
+
+	var endpoint = url.URL{
+		Scheme: "http",
+		Path:   "/api/apps",
+		Host:   host,
+	}
+
+	fmt.Printf("processing file [%s]\n", inputFileName)
+
+	f, err := os.Open(inputFileName)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer f.Close()
+
+	r := bufio.NewReader(f)
+	line, err := r.ReadString(10) // line defined once
+	for err != io.EOF {
+
+		if len(strings.TrimSpace(line)) != 0 {
+
+			start := 0
+			num := min(numOfApps, MAX_APPS_PER_CALL)
+			v := url.Values{
+				"fullDetail": {"true"},
+				"start":      {strconv.Itoa(start)},
+				"num":        {strconv.Itoa(num)},
+			}
+
+			for start < numOfApps {
+				if runType == "search" {
+					v.Set("q", strings.TrimSpace(line))
+				} else {
+					v.Set("category", strings.TrimSpace(line))
+				}
+
+				resp := &AppSearchResponse{}
+				if err = httpGetJSON(endpoint, v, resp); err != nil {
+					return nil, err
+				}
+
+				for _, currApp := range resp.Results {
+					if _, ok := appsHash[currApp.AppID]; !ok {
+						appsHash[currApp.AppID] = true
+						upd, err := convertDate(currApp.Updated)
+						if err != nil {
+							fmt.Printf("convertDate [%s] err: %v", currApp.Updated, err)
+						} else {
+							currApp.Updated = upd
+						}
+						apps = append(apps, currApp)
+					}
+				}
+
+				start += num
+				num = min(numOfApps-start, MAX_APPS_PER_CALL)
+				v.Set("start", strconv.Itoa(start))
+				v.Set("num", strconv.Itoa(num))
+			}
+		}
+
+		line, err = r.ReadString(10) //  line was defined before
+	}
+	return apps, nil
+}
+
+func httpGetJSON(u url.URL, v url.Values, target interface{}) error {
 
 	retry := 0
 
-	r, err := http.Get(url)
+	u.RawQuery = v.Encode()
+	fmt.Printf("calling google play api [%s]\n", u.String())
+	r, err := http.Get(u.String())
 	for httpError := isAPIError(r, err); httpError != nil; {
 		if retry > 3 {
 			return err
 		}
-		fmt.Printf("http.Get error [%s] ... retrying in 10 seconds\n", err.Error())
+		if err != nil {
+			fmt.Printf("http.Get error [%s] ... retrying in 10 seconds\n", err.Error())
+		} else {
+			fmt.Printf("http.Get response code [%d] ... retrying in 10 seconds\n", r.StatusCode)
+		}
 		time.Sleep(10 * time.Second)
 		retry++
-		r, err = http.Get(url)
+		r, err = http.Get(u.String())
 	}
 	defer r.Body.Close()
 
@@ -174,4 +226,11 @@ func convertDate(strDate string) (string, error) {
 		return "", err
 	}
 	return fmt.Sprintf("%.4d-%.2d-%.2d", year, month, day), nil
+}
+
+func min(a, b int) int {
+	if a <= b {
+		return a
+	}
+	return b
 }
